3358e3a48942e1e381dcf5a2a9901b11a91fca0d
Minh Vu <vnm241191@gmail.com>
2017-01-17 08:52:05 -0800

    Changing the New Activity

--- app/src/main/java/tcss450/uw/edu/testinggitproject/newActivity.java
+++ app/src/main/java/tcss450/uw/edu/testinggitproject/newActivity.java
@@ -2,6 +2,7 @@ package tcss450.uw.edu.testinggitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class newActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class newActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        Toast.makeText(this, "New Activity", Toast.LENGTH_LONG).show();
     }
 }
